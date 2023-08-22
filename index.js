907f3d95d4f1844f928df5854767b1c0737af371
ryan <1942kg@gmail.com>
2023-08-22 13:08:07 +0900

    init--3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log("test");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
-console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
