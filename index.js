c434c056913654cae7c4d831f93cc935a0fb732e
ryan <1942kg@gmail.com>
2023-08-22 13:27:24 +0900

    feat(): hooks + git cz
    hooks+git cz commit msg
    
    BREAKING CHANGE: undefined

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
 console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
-console.log("aaaabbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa");
